382260147b6ddc3ef20f65357aca84e46b424059
dilmurodxudoyberdiyev <dxudoyberdiyev041@gmail.com>
2022-10-24 00:37:20 +0500

    js uy ishi5

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let a = +prompt("birinchi sonni kiriting");
+let b = prompt("amalni kiriting");
+let c = +prompt("ikkinchi sonni kiriting");
+
+switch(b){
+    case "+":
+        natija = a + c;
+        break
+
+        case "-":
+        natija = a - c;
+        break
+
+        case "*":
+        natija = a * c;
+        break
+
+        case "/":
+        natija = a / c;
+        break
+
+
+}
+
+console.log(natija);
